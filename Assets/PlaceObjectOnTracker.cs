2b83a0647c70aa3bd39f061533053299ae26c3c1
LEGEARD Anthony <77632799+Virudi100@users.noreply.github.com>
2022-04-06 17:08:45 +0200

    truc a faire, voir screen dans cs

---
+++ Assets/PlaceObjectOnTracker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaceObjectOnTracker : MonoBehaviour
+{
+    public ARTrackedImageManager manager;
+    public GameObject cubePrefab;
+
+    private void OnEnable()
+    {
+        manager.trackedImagesChanged += OnTrackedImageChange;
+    }
+
+    private void OnDisable()
+    {
+        manager.trackedImagesChanged -= OnTrackedImageChange;
+    }
+
+    private void OnTrackedImageChange(ARTrackedImagesChangedEventArgs args)
+    {
+        for(int i = 0;i < args.added.Count;i++)
+        {
+            Instantiate(cubePrefab, args.added[i].transform);
+        }
+    }
+}
